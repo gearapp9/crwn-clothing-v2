2b70e89eb191d7e3f935311d30bcee483da746d2
gearapp9 <kadrimalek103@gmail.com>
2024-05-22 18:28:27 +0200

    using reselect library on the category selector to prevent re-rendering issues

--- src/store/categories/categories-selector.js
+++ src/store/categories/categories-selector.js
@@ -1,6 +1,19 @@
-export const selectCategories = (state) => state.categories.categories.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot;
-    acc[title.toLowerCase()] = items;
-    return acc;
-    
-  }, {});;
+import { createSelector } from "reselect";
+
+const categoriesSlice = (state) => state.categories;
+
+const categoriesSelectorMemo = createSelector(
+  [categoriesSlice],
+  (categoriesMemo) => categoriesMemo.categories
+);
+
+export const selectCategories = createSelector(
+  [categoriesSelectorMemo],
+  (categories) => {
+    categories.reduce((acc, docSnapshot) => {
+      const { title, items } = docSnapshot;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
